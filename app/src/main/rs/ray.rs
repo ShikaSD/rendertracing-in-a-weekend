ed2860c6cffca00de075b45b1ffd8e9195c493c9
Andrei Shikov <andrei.shikov@corp.badoo.com>
2019-03-26 01:01:21 +0000

    Two elements with normals

--- app/src/main/rs/ray.rs
+++ app/src/main/rs/ray.rs
@@ -1,6 +1,9 @@
 #pragma version(1)
 #pragma rs java_package_name(com.shika.rayrender)
 
+#define FLT_MAX 3.402823466e+38F
+#define FLT_MIN 1.175494351e-38F
+
 const float3 lower_left_corner = {-2, -1, -1};
 const float3 horizontal = {4, 0, 0};
 const float3 vertical = {0, 2, 0};
@@ -11,28 +14,87 @@ typedef struct {
     float3 direction;
 } ray;
 
+typedef struct {
+    float3 center;
+    float radius;
+} sphere;
+
+typedef struct {
+    float dist;
+    float3 p;
+    float3 normal;
+} record;
+
+typedef enum {
+    obj_sphere
+} obj;
+
+typedef union {
+    sphere sphere;
+} hitable;
+
+typedef struct {
+    obj type;
+    hitable value;
+} hitable_t;
+
 static float3 ray_at(const ray *r, float t) {
     return r->origin + r->direction * t;
 }
 
-static float hit_sphere(float3 center, float radius, const ray *r) {
-    float3 d_oc = r->origin - center;
+static bool hit_sphere(const sphere *s, const ray *r, record *rec, float dist_min, float dist_max) {
+    float3 d_oc = r->origin - s->center;
     float a = dot(r->direction, r->direction);
     float b = 2 * dot(d_oc, r->direction);
-    float c = dot(d_oc, d_oc) - radius * radius;
+    float c = dot(d_oc, d_oc) - s->radius * s->radius;
     float d = b * b - 4 * a * c;
-    if (d < 0) {
-        return -1;
-    } else {
-        return (-b - sqrt(d)) / (2 * a);
+
+    if (d >= 0) {
+        float dist = (-b - sqrt(b * b - a * c)) / a;
+        if (dist > dist_max || dist < dist_min) {
+            dist = (-b + sqrt(b * b - a * c)) / a;
+        }
+        if (dist < dist_max && dist > dist_min) {
+            rec->dist = dist;
+            rec->p = ray_at(r, dist);
+            rec->normal = (rec->p - s->center) / s->radius; // normalize
+            return true;
+        }
     }
+    return false;
 }
 
-static float3 color(const ray *r) {
-    float3 center = { 0, 0, -1 };
-    float dist = hit_sphere(center, 0.5, r);
-    if (dist > 0) {
-        float3 normal = normalize(ray_at(r, dist)) - center;
+static bool hit_list(const hitable_t *h, int hsize, const ray *r, record **rec, float dist_min, float dist_max) {
+    float closest = dist_max;
+    record t_rec;
+    bool hit_result = false;
+
+    for (int i = 0; i < hsize; i++) {
+        hitable_t current = *(h + i);
+
+        bool hit = false;
+        switch (current.type) {
+            case obj_sphere:
+                hit = hit_sphere(&current.value.sphere, r, &t_rec, dist_min, closest);
+                break;
+        }
+
+        if (hit) {
+            closest = t_rec.dist;
+            hit_result = true;
+            *rec = &t_rec;
+        }
+    }
+
+    return hit_result;
+}
+
+static float3 color(const ray *r, const hitable_t *world) {
+    record* rec = NULL;
+    bool got_hit = hit_list(world, 2, r, &rec, 0, FLT_MAX);
+
+    if (got_hit) {
+        float3 normal = rec->normal;
         return 0.5 * (float3) { normal.x + 1, normal.y + 1, normal.z + 1 };
     }
 
@@ -46,12 +108,20 @@ uchar4 RS_KERNEL raytrace(uchar4 in, rs_kernel_context context, int32_t x, int32
     int32_t width = rsGetDimX(context);
     int32_t height = rsGetDimY(context);
 
+    hitable s_h = { { { 0, 0, -1 }, 0.5 } };
+    hitable g_h = { { { 0, -100.5, -1 }, 100 } };
+    
+    hitable_t world[2] = {
+        { .type = obj_sphere, .value = s_h },
+        { .type = obj_sphere, .value = g_h }
+    };
+
     float u = ((float) x) / width;
     float v = 1 - ((float) y) / height;
 
     float3 direction = lower_left_corner + u * horizontal + v * vertical;
     ray r = { origin, direction };
-    float3 col = color(&r);
+    float3 col = color(&r, world);
 
     input.r = col.r;
     input.g = col.g;
